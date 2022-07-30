2458fb6d3167615568ae74145d34f5ecb0c9359d
Luis Hutterli <lshutterli@gmail.com>
2022-07-30 13:57:56 +0200

    Cleaning + imporovments
    Each thread will now compute multiple lines instead of only on line per thread

--- src/main/java/ch/lsh/App.java
+++ src/main/java/ch/lsh/App.java
@@ -3,6 +3,7 @@ package ch.lsh;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.util.Precision;
@@ -34,48 +35,47 @@ public class App {
         double yc = 0d;
         double size = 2d;
 
-        int n = 20_000; // create n-by-n image
-        int max = 255; // maximum number of iterations
+        int n = 16_000; // create n-by-n image
+        int max = 100; // maximum number of iterations
+
+        int maxThreads = 20;
+        int linesPerThread = 20;
+        int pollingRate = 2;
+
+
+
+        if(n % linesPerThread != 0) throw new PictureException("Size has to be a multiple of linesPerThread(lpt): n=x*lpt or lpt%n=0");        
 
         long startTime = System.currentTimeMillis();
 
         GrayCanvas canvas = new GrayCanvas(n, n, max);
 
-        // for(int y = 0; y < canvas.getHeight(); y++) {
-        // for(int x = 0; x < canvas.getWidth(); x++) {
-        // Complex z = new Complex(xc - size/2 + size*x/canvas.getWidth(), yc - size/2 +
-        // size*y/canvas.getHeight());
-        // canvas.setPixel(x, y, max - mandelbrot(z, max));
-        // }
-        // if(y % 10 == 0) showStatus(y,canvas.getHeight(), startTime);
-        // }
-
         ArrayList<MandelbrotThread> threads = new ArrayList<>();
-        int maxThreads = 24;
-
         int next_y = 0;
         int index = 0;
-
         int completed = 0;
         while (next_y < canvas.getHeight()) {
             if (threads.size() < maxThreads) {
-                MandelbrotThread th = new MandelbrotThread(next_y, 0, canvas.getWidth(), max, canvas.getWidth(),
+                MandelbrotThread th = new MandelbrotThread(next_y, next_y + linesPerThread, max, canvas.getWidth(),
                         canvas.getHeight(), xc, yc, size);
                 th.start();
                 threads.add(th);
-                next_y++;
+                next_y += linesPerThread;
             }
-            if (index % 10 == 0) {
+            if (index % pollingRate == 0) {
                 for (int i = 0; i < threads.size(); i++) {
                     if (!threads.get(i).isAlive()) {
                         MandelbrotThread th = threads.get(i);
                         threads.remove(i);
-                        int[] results = th.getResult();
-                        int y = th.getY();
-                        for (int x = th.getXStart(); x < th.getXFinish(); x++) {
-                            canvas.setPixel(x, y, results[x]);
+                        int[][] results = th.getResult();
+
+                        // System.out.println(Arrays.deepToString(results));
+                        for (int y = th.getY_start(); y < th.getY_finish(); y++) {
+                            for (int x = 0; x < canvas.getWidth(); x++) {
+                                canvas.setPixel(x, y, results[y-th.getY_start()][x]);
+                            }
                         }
-                        completed++;
+                        completed += linesPerThread;
                         showStatus(completed, canvas.getHeight());
                     }
                 }
@@ -89,19 +89,29 @@ public class App {
         for (int i = 0; i < threads.size(); i++) {
             MandelbrotThread th = threads.get(i);
             th.join();
-            int[] results = th.getResult();
-            int y = th.getY();
-            for (int x = th.getXStart(); x < th.getXFinish(); x++) {
-                canvas.setPixel(x, y, results[x]);
+            int[][] results = th.getResult();
+            for (int y = th.getY_start(); y < th.getY_finish(); y++) {
+                for (int x = 0; x < canvas.getWidth(); x++) {
+                    canvas.setPixel(x, y, results[y-th.getY_start()][x]);
+                }
             }
         }
 
         long elapsedTime = System.currentTimeMillis() - startTime;
         System.out.println("Elapsed Time: " + (elapsedTime / 1000f) + "s");
         SaveUtil.saveGrayCanvas(canvas, new File("/home/lsh/Desktop/" + n + ".threading.pgm"));
+
+        // for(int y = 0; y < canvas.getHeight(); y++) {
+        // for(int x = 0; x < canvas.getWidth(); x++) {
+        // Complex z = new Complex(xc - size/2 + size*x/canvas.getWidth(), yc - size/2 +
+        // size*y/canvas.getHeight());
+        // canvas.setPixel(x, y, max - mandelbrot(z, max));
+        // }
+        // if(y % 10 == 0) showStatus(y,canvas.getHeight(), startTime);
+        // }
     }
 
     private static void showStatus(int y, int height) {
-        System.out.println(y + "/" + height + "\t" + Precision.round(((double) y) / height * 100d, 2) + "%" + "\t");
+        System.out.println(y + "/" + height + "\t" + Precision.round(((double) y) / height * 100d, 3) + "%" + "\t");
     }
 }
--- src/main/java/ch/lsh/MandelbrotThread.java
+++ src/main/java/ch/lsh/MandelbrotThread.java
@@ -4,9 +4,8 @@ import org.apache.commons.math3.complex.Complex;
 
 public class MandelbrotThread extends Thread {
 
-    private int y;
-    private int x_start;
-    private int x_finish;
+    private int y_start;
+    private int y_finish;
     private int maxval;
 
     private int width;
@@ -16,13 +15,13 @@ public class MandelbrotThread extends Thread {
     private double yc;
     private double size;
 
-    private int[] result;
+    private int[][] result;
 
-    public MandelbrotThread(int y, int x_start, int x_finish, int maxval, int width, int height, double xc, double yc,
+    public MandelbrotThread(int y_start, int y_finish, int maxval, int width, int height,
+            double xc, double yc,
             double size) {
-        this.y = y;
-        this.x_start = x_start;
-        this.x_finish = x_finish;
+        this.y_start = y_start;
+        this.y_finish = y_finish;
         this.maxval = maxval;
         this.width = width;
         this.height = height;
@@ -30,14 +29,18 @@ public class MandelbrotThread extends Thread {
         this.yc = yc;
         this.size = size;
 
-        result = new int[width];
+        result = new int[y_finish-y_start][width];
     }
 
     @Override
     public void run() {
-        for(int x = x_start; x < x_finish; x++) {
-            Complex z = new Complex(xc - size/2 + size*x/width, yc - size/2 + size*y/height);
-            result[x] = maxval - mandelbrot(z, maxval);
+        for (int y = y_start; y < y_finish; y++) {
+            // System.out.println(y);
+            for (int x = 0; x < width; x++) {
+                Complex z = new Complex(xc - size / 2 + size * x / width, yc - size / 2 + size * y / height);
+                result[y-y_start][x] = maxval - mandelbrot(z, maxval);
+                // System.out.println(this.getName() + ": " + x + " " + y + " " + result[y][x]);
+            }
         }
         super.run();
     }
@@ -53,19 +56,15 @@ public class MandelbrotThread extends Thread {
         return max;
     }
 
-    public int[] getResult() {
+    public int[][] getResult() {
         return result;
     }
 
-    public int getXStart() {
-        return x_start;
+    public int getY_start() {
+        return y_start;
     }
 
-    public int getXFinish() {
-        return x_finish;
-    }
-
-    public int getY() {
-        return y;
+    public int getY_finish() {
+        return y_finish;
     }
 }
